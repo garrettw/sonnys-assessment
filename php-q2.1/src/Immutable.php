a26d0be6e57c06e6238ab041c347bed167439e1d
Garrett W <garrettw87@gmail.com>
2023-01-26 02:30:25 -0600

    Create Immutable.php

---
+++ php-q2.1/src/Immutable.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+trait Immutable
+{
+    /**
+     * This reusable method provides a simple API for "modifying" properties of immutable objects by
+     * returning a new copy with the desired value(s) changed.
+     *
+     * Known issue: this uses a mechanism that does not call the class constructor to make a new object,
+     * which means that changed properties cannot have their new values type-checked by the
+     * constructor. There are several ways that type-checking could be accomplished, but I don't
+     * want to go too deep down that rabbit hole here as I think it's probably overkill for this
+     * task and I've probably gone plenty far already.
+     *
+     * @param array $newProps the list of properties to be changed (as keys) with their new values
+     */
+    public function with(array $newProps)
+    {
+        $newObj = clone $this;
+        foreach ($newProps as $prop => $val) {
+            if (property_exists($newObj, $prop)) {
+                $newObj->$prop = $val;
+            }
+        }
+        return $newObj;
+    }
+}
