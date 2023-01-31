fcf15af73a1d101ff07c8d4a839fb5da72e5d7ad
Garrett W <garrettw87@gmail.com>
2023-01-30 20:02:51 -0600

    Update RemoteUser.php

--- php-q2.3/src/RemoteUser.php
+++ php-q2.3/src/RemoteUser.php
@@ -4,44 +4,22 @@ namespace App;
 
 use App\Provided\RemoteControl;
 
+/**
+ * We could have a constructor here where we pass in a remote for the user to "hold",
+ * along with methods to pick up and drop remotes, but I think it makes more sense
+ * for the user here to be immutable and interact with a given remote in a one-off
+ * manner when told to. I would feel differently if the user could, say, only
+ * interact with one remote at a time, and could only perform certain interactions
+ * if exclusive access to the object were granted.
+ * This method could, in theory, be marked as static, but I generally avoid that
+ * because it can impact testability and future flexibility. If for some reason
+ * the remote needed to know about the user in order to communicate back to it, the
+ * user (as $this) could be passed to the remote here (but only if not static).
+ */
 class RemoteUser
 {
-    /** @var RemoteControl|null */
-    private $heldRemote;
-    
-    /**
-     * We have the option of constructing a user with a remote already in his hand, but that is optional.
-     */
-    public function __construct(?RemoteControl $remote = null)
+    public function pressPowerOn(RemoteControl $remote)
     {
-        $this->heldRemote = $remote;
-    }
-    
-    /**
-     * We can have the user press the Power On button of a remote he already holds, or of another arbitrary remote.
-     */
-    public function pressPowerOn(?RemoteControl $remote = null)
-    {
-        $remote = $remote ?? $this->heldRemote;
-        if (!isset($remote)) {
-            throw new \LogicException('No remote present!');
-        }
         $remote->powerOn();
-    }
-    
-    /**
-     * Pick up a new remote, or replace the one in hand with a different one.
-     */
-    public function holdRemote(RemoteControl $remote)
-    {
-        $this->heldRemote = $remote;
-    }
-    
-    /**
-     * Drop the remote currently in hand.
-     */
-    public function dropRemote()
-    {
-        $this->heldRemote = null;
     }
 }
